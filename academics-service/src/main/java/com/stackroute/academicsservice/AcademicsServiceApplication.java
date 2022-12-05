6e62d8e73a9f4ff895cfc247b526099eb3ade98d
shubhamrajput <shubhamrajput1969@gmail.com>
2022-12-05 11:19:52 +0530

    Service Registry using Eureka Server comple.

--- academics-service/src/main/java/com/stackroute/academicsservice/AcademicsServiceApplication.java
+++ academics-service/src/main/java/com/stackroute/academicsservice/AcademicsServiceApplication.java
@@ -3,9 +3,11 @@ package com.stackroute.academicsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
+@EnableEurekaClient
 public class AcademicsServiceApplication {
 
 	public static void main(String[] args) {
