82f9f9049bce33738ce0d45b4cd72fc4a474e5c4
khan.adan.04 <khan.adan.04@gmail.com>
2022-12-12 11:59:02 +0530

    updated

--- product-webapp/webapp/src/app/profilesearch/profilesearch.component.ts
+++ product-webapp/webapp/src/app/profilesearch/profilesearch.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+interface Location {
+  value: string;
+  viewValue: string;
+}
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profilesearch',
@@ -6,8 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilesearch.component.css']
 })
 export class ProfilesearchComponent {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
+  constructor(private formBuilder:FormBuilder){}
+  profileForm = this.formBuilder.group({});
+  pref_loc: Location[] = [
+    {value: 'Hyderabad', viewValue: 'Hyderabad'},
+    {value: 'banglr', viewValue: 'Banglore'},
+    {value: 'Banglore', viewValue: 'Chennai'},
+    {value: 'Mumbai', viewValue: 'Mumbai'},
+    {value: 'Pune', viewValue: 'Pune'},
+    {value: 'Delhi', viewValue: 'Delhi'},
+    {value: 'Noida', viewValue: 'Noida'},
+    {value: 'Kolkata', viewValue: 'Kolkata'}
+  ];
+  pref_exp: Location[] = [
+    {value: 'Fresher', viewValue: 'Fresher'},
+    {value: '0 - 1', viewValue: '0 - 1'},
+    {value: '1 - 2', viewValue: '1 - 2'},
+    {value: '2 - 3', viewValue: '2 - 3'},
+    {value: '3 - 4', viewValue: '3 -4'},
+    {value: '4 - 5', viewValue: '4 - 5'},
+    {value: '5+', viewValue: '5+'},
+  ]
+
 
 }
+export class PaginatorOverviewExample {}
